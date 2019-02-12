e937505d5372f367fc85602c7e72ba321e0214ea
thanhtran18 <tranct@myumanitoba.ca>
2019-02-12 00:16:42 -0600

    Added log in page

---
+++ Uni./client/src/screens/components/LogInForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    TextInput,
+    KeyboardAvoidingView,
+    StatusBar
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../../components/StyledText';
+
+export default class LogInForm extends React.Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content"/>
+                <TextInput
+                    style={styles.input}
+                    placeholder="username"
+                    placeholderTextcolor="rgba(255,255,255,0.7)"
+                    returnKeyType="next"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    onSubmitEditing={() => this.passwordInput.focus()}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="password"
+                    placeholderTextcolor="rgba(255,255,255,0.7)"
+                    secureTextEntry
+                    returnKeyType="go"
+                    ref={(input) => this.passwordInput = input}
+                />
+
+                <TouchableOpacity style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>LOG IN</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        marginBottom: 10,
+        color: '#FFF',
+        paddingHorizontal: 10,
+    },
+    buttonContainer: {
+        backgroundColor: '#2980b9',
+        paddingVertical: 15
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontWeight: '700'
+    }
+});
