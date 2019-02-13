ea503d47d15fc69cf9711ebca60c3d4d54c743c9
Buhle Mwanza <buhle.mwanza@gmail.com>
2019-02-13 14:53:34 -0600

    Added event image

---
+++ Uni./client/src/screens/CurrentActivitiesScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    KeyboardAvoidingView
+} from 'react-native';
