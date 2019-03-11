ac6966e4597e6a29abd09f24605e246b24f32671
Rodni <Kikanye@users.noreply.github.com>
2019-03-11 16:37:48 -0500

    Implement authentication and validation functionality for endpoints
    * Changed the database which the signup endpoint uses.
    
    * Current changes to the backend code, code includes current working signup. Currently unfinished code for validation
    
    * Full server side functions for signup, sign-in, password hashing(salt + hash), token generation, sending token to user upon login, and mongoose storage
    
    * Changed the unauthorized response for tokens to a custom message
    
    * Made all API responses custom as required.
    
    * Endpoints to support getting and adding of activities.
    
    Todo: 
    * Add username to users and save to database
    * Make sure the data base is required for signup
    * Make sure the username is unique

--- Uni./backend/Uni./app.js
+++ Uni./backend/Uni./app.js
@@ -1,36 +1,36 @@
 'use strict'
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const passportConf = require('./passport');
+var mongoose = require('mongoose');
 
-var express = require("express");
+
+mongoose.connect("mongodb://testUser:testUser@cluster0-shard-00-00-twf8g.mongodb.net:27017," +
+    "cluster0-shard-00-01-twf8g.mongodb.net:27017,cluster0-shard-00-02-twf8g.mongodb.net:27017/testing?" +
+    "ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true });
+
+
 var app = express();
-var routes = require("./routes")
-
-var jsonParser = require("body-parser").json;
-var logger = require("morgan");
-
-app.use(logger("dev"));
-app.use(jsonParser());
-
-app.use("/users", routes);
-
-//catch 404 and forward to error handler
-app.use(function(req, res, next){
-  var err = new Error("Not Found");
-  next(err);
-});
 
 
-//Error handler
-app.use(function(err, req, res, next){
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      message: err.message
-    }
-  })
-});
+//middleware
+//middlewares are run in sequence
+app.use(morgan('dev'));//morgan logs the calls to the routes
+app.use(bodyParser.json());//parsing the json object
 
+//routes
+app.use('/users', require('./routes/users'));
+app.use('/activities', require('./routes/activities'));
+
+//start the server 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
     console.log("Express server is listening on port", port);
 })
+
+//Todo: Add username to users and save to database
+//Todo: Make sure the data base is required for signup
+//Todo: Make sure the username is unique
+//Todo: Include appropriate error and success messages
--- Uni./backend/Uni./routes/users.js
+++ Uni./backend/Uni./routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = require('express-promise-router')();
+const passport = require('passport');
+const passportConf = require('../passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const {validateBody, schemas, activitySchema} = require('../helpers/routeHelpers');
+//const {validateBody, activitySchema} = require('../helpers/routeHelpers');
+const UsersController = require('../controllers/users');
+const activityController = require('../controllers/activities');
 
-module.exports = router;
+router.route('/signup')
+//validate makes sure the data is valid 
+//if not it sends a response without calling the usersController
+  .post(validateBody(schemas.authSchema),UsersController.signUp);
+
+router.route('/signin')
+  .post(validateBody(schemas.authSchema), UsersController.signIn);
+
+router.route('/secret')
+  .get(UsersController.secret);
+
+  module.exports = router;
