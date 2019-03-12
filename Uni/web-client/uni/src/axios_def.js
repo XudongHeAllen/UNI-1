323f1cafc56e3aea05d1b1a691bc2bfd1ee93f5a
Qi Wen <wenq@myumanitoba.ca>
2019-03-11 20:10:32 -0500

    Moved files and directories to match the new directory structure

--- Uni/web-client/uni/src/axios_def.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'http://localhost:3000'//the default server url. need to know mongoDB data structure as well.
-});
-
-export default instance;
--- Uni/web-client/uni/src/components/pages/SignUpPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-//this is a function component
-const SignUpPage = () => (
-    <h1>this is a sign up Page</h1>
-);
-
-export default SignUpPage
