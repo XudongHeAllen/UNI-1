7e97068f592697b29ae36d28c9fcb25e80c75c10
Qi Wen <wenq@myumanitoba.ca>
2019-03-21 23:47:02 -0500

    Message that shows current activity is full

---
+++ web-client/src/components/messages/ActivityIsFull.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ActivityIsFull = () => (
+    <div className="ui warning message">
+        <i className="close icon"/>
+        <div className="header">
+            You must register before you can do that!
+        </div>
+        Visit our registration page, then try again
+    </div>
+);
+
+export default ActivityIsFull
+
+
