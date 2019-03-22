d07b8d6a87dd922ed29649eacbc8c86798378960
Qi Wen <wenq@myumanitoba.ca>
2019-03-21 19:57:33 -0500

    put method does not work

--- web-client/src/components/pages/UserActivities.js
+++ web-client/src/components/pages/UserActivities.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import Modal from 'react-awesome-modal';
 import '../../App.css';
+import axios from "../../axios_def";
 
 class UserActivities extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            visible : false
+            visible : false,
+            attend : false,
+            full : false
         }
+
+        //TODO: how to know the creator of this activity, maybe need to change the structure of the database？
+
+
     }
 
     openModal() {
+        console.log("Iam user: "+this.props.userID);
+        //console.log(this.props.attendances.length);
+        //console.log(this.props.attendances);
+        //console.log(this.props.attendances[0]);
+
+        let attend = false;
+        let attendanceCount = this.props.attendances.length;
+        let index = 0;
+
+        while ( attendanceCount > 0){
+            if(this.props.attendances[index] === this.props.userID){
+                attend = true;
+                console.log("success");
+            }
+
+            index = index + 1;
+            attendanceCount = attendanceCount - 1;
+        }
+
         this.setState({
-            visible : true
+            visible : true,
+            attend: attend
         });
     }
 
@@ -22,7 +49,57 @@ class UserActivities extends React.Component{
         });
     }
 
+    joinActivityHandler = () => {
+        //const id = this.props.activityID;
+        // const token = this.props.token;
+        //
+        // console.log(token);
+        //
+        // const helper= {
+        //     headers: {"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVTkkiLCJzdWIiOiI1YzhmZmFhMGNkZDUyNDA4N2U3ZTNhNmYiLCJpYXQiOjE1NTMyMTMyODY0OTgsImV4cCI6MTU1MzI5OTY4NjQ5OH0.YVt982StxFLfesEka4_pYN8fm6221JEqiDNn7OtHSXo",
+        //         "Content-Type": "application/json"}
+        // };
+        //
+        // axios.put('/activities/activity/attend/5c9176bacdd524087e7e3a9e', { headers: {"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJVTkkiLCJzdWIiOiI1YzhmZmFhMGNkZDUyNDA4N2U3ZTNhNmYiLCJpYXQiOjE1NTMyMTMyODY0OTgsImV4cCI6MTU1MzI5OTY4NjQ5OH0.YVt982StxFLfesEka4_pYN8fm6221JEqiDNn7OtHSXo",
+        //         "Content-Type": "application/json"}}).then(res => {
+        //     console.log(res.data);
+        // }).catch((error) => {
+        //     console.log(error);
+        // });
+        const token = this.props.token;
+
+        console.log("this is the token: "+token);
+
+        const helper= {
+            headers: {"Authorization": '' + token}
+        };
+
+        axios.get('/users/user/activities/attending',helper).then(res => {
+            const activities = res.data.activities;
+            console.log(activities);
+            console.log(token);
+        }).catch((error) => {
+            console.log(error);
+        });
+
+
+        console.log("this is the end");
+
+        // axios.put('/activities/activity/attend/5c9176bacdd524087e7e3a9e',helper).then(res => {
+        //     const activities = res.data.activities;
+        //     console.log(activities);
+        //     console.log(token);
+        // }).catch((error) => {
+        //     console.log(error);
+        // });
+
+        //console.log(this.props.activityID);
+        //console.log("joined");
+    };
+
+
     render() {
+
         return (
             <div className='item'>
                 <div className="content">
@@ -74,11 +151,22 @@ class UserActivities extends React.Component{
 
                             <br/>
 
-                            <button
-                                className='small ui primary button'
-                                onClick={() => this.closeModal()}>
-                                Join
-                            </button>
+                            {!this.state.attend && (
+                                <button
+                                    className='small ui primary button'
+                                    onClick={this.joinActivityHandler}>
+                                    Join
+                                </button>
+                            )}
+
+                            {this.state.attend && (
+                                <button
+                                    className='small ui primary button'
+                                    onClick={() => this.closeModal()}>
+                                    unJoin
+                                </button>
+                            )}
+
                             <button
                                 className='small ui primary button'
                                 onClick={() => this.closeModal()}>
